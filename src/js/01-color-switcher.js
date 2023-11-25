097cdcef47930b3ad7073722d59c39ef580972af
ValeriaSihr <132007746+ValeriaSihr@users.noreply.github.com>
2023-11-25 21:40:43 +0000

    task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const mainChangeColor = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+let interval = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+btnStop.setAttribute('disabled', '');
+btnStart.addEventListener('click', element => {
+  element.target.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+  interval = setInterval(() => {
+    mainChangeColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+btnStop.addEventListener('click', element => {
+  element.target.setAttribute('disabled', true);
+  btnStart.removeAttribute('disabled');
+  clearInterval(interval);
+});
